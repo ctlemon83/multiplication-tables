c955b14aaf53b1e36a660d2c39bd34cfb7f0c5c4
Colin Lemon <ctlemon83@gmail.com>
2020-09-08 14:35:40 -0500

    Removed 0 and 1 from array

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var factorArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var factorArr = [2, 3, 4, 5, 6, 7, 8, 9];
 var validation = document.getElementById("corIncor");
 
 // Chooses a random digit from the array
